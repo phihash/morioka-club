eb9b88c7b64e0f52d68af4ec2c258b6e5b4703df
phihash <insharp0220@gmail.com>
2024-02-11 17:12:50 +0900

    Headerコンポーネント作成

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+
+const page = () => {
+  return (
+    <>
+    <Header></Header>
+      <p className='text-amber-500'>ssss</p>
+      page
+      </>
+  )
+}
+
+export default page

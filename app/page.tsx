f6bc02026006b1f9c860c57b0f3462a0a8bd9ebd
phihash <insharp0220@gmail.com>
2024-02-13 17:41:42 +0900

    page微修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Image src={localImage} alt="美味しそうなお寿司" />
-      <p className="mt-8 text-center text-lg font-bold">すし清次郎 フェザン店</p>
+      <p className="flex justify-center mt-8 text-center text-lg font-bold">すし清次郎 フェザン店</p>
     </div>
 
   );

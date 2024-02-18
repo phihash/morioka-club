bbe69aef8ee6e871fbdeeac527d46d3fb142a362
phihash <insharp0220@gmail.com>
2024-02-19 04:12:49 +0900

    headerの余白調整

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,7 +4,7 @@ import { fredoka ,noto_sans_jp ,m_plus_1p } from '../fonts'
 
 const Navigation : React.FC<NavigationProps>  = ({activeTab}) => {
   return (
-    <nav className="ml-6 text-sm">
+    <nav className="ml-8 text-sm">
         <div className='flex'>
           <div className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 border-b-4  border-b-lime-600 `)}>
             <Link href="/about">盛岡について</Link>

30ba307e971317f076de052abfc932f5e4c1021b
phihash <insharp0220@gmail.com>
2024-02-20 12:17:49 +0900

    微修正

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -10,20 +10,20 @@ const Navigation : React.FC  = () => {
     setActiveTab(e.currentTarget.dataset.tab)
   }
   return (
-    <nav className="ml-8 text-sm">
+    <nav className="text-sm">
       <div className='flex overflow-x-auto'>
         <Link href="/about">
-          <div data-tab="about" onClick={clickTab} className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 w-max flex-none ${activeTab == "about" ? "border-b-4 border-b-lime-600 " : ""}  `)}>
+          <div data-tab="about" onClick={clickTab} className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 w-max flex-none ${activeTab == "about" ? "border-b-2 border-b-lime-600 text-slate-800" : ""}  `)}>
               盛岡について
           </div>
           </Link>
           <Link href="/food">
-          <div data-tab="food" onClick={clickTab} className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 w-max flex-none  ${activeTab == "food" ? "border-b-4 border-b-lime-600 " : ""} `)}>
+          <div data-tab="food" onClick={clickTab} className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 w-max flex-none  ${activeTab == "food" ? "border-b-2 border-b-lime-600 text-slate-800" : ""} `)}>
               食べ物
           </div>
           </Link>
           <Link href="/link">
-          <div data-tab="link" onClick={clickTab} className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 w-max flex-none ${activeTab == "link" ? "border-b-4 border-b-lime-600 " : ""} `)}>
+          <div data-tab="link" onClick={clickTab} className={(`${noto_sans_jp.className} font-semibold text-slate-500 hover:text-slate-800 p-3 w-max flex-none ${activeTab == "link" ? "border-b-2 border-b-lime-600 text-slate-800" : ""} `)}>
               リンク
           </div>
           </Link>

1f76d08a5ba572d087f8c69ce93c79cb34772d0f
phihash <insharp0220@gmail.com>
2024-02-24 19:17:34 +0900

    foodページにfoodコンポーネント表示、未完成

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { fredoka } from '../fonts'
+import FoodContentBox from '../components/foodContentBox'
 
 const page = () => {
   return (
-    <div>page</div>
+  <div className='bg-lime-50'>
+    <div className='w-9/12 mx-auto pt-12'>
+      <h2 className={`${fredoka.className} text-4xl font-semibold`}>Food</h2>
+    </div>
+    <FoodContentBox></FoodContentBox>
+  </div>
   )
 }
 

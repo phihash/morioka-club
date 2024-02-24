e4457b99f39cbbfebe7ecac6020c699e4c34cc9f
phihash <insharp0220@gmail.com>
2024-02-25 02:46:49 +0900

    topページの写真削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
-import localImage from "../public/sushi.png";
 
 export default function Home() {
   return (
     <div>
-      <Image src={localImage} alt="美味しそうなお寿司" />
-      <p className="flex justify-center mt-8 text-center text-lg font-bold">すし清次郎 フェザン店</p>
     </div>
 
   );

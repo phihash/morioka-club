f3a3f8c08a8971f096b9a29a9fca996841d4e562
phihash <insharp0220@gmail.com>
2024-02-29 05:51:46 +0900

    カテゴリごとにタップすると,それに該当するおみやげが表示されるようにした。画像や中身については未完成

--- app/souvenir/page.tsx
+++ app/souvenir/page.tsx
@@ -3,12 +3,52 @@ import React from "react";
 import SouvenirButton from "../components/souvenirButton";
 import { useState } from "react";
 import { noto_sans_jp } from "../fonts";
+import SouvenirItem from "../components/souvenirItem";
 
 const SouvenirPage = () => {
-  const [selectedCategories, setSelectedCategories] = useState<string[]>(["甘い","スナック系","個包装","あんこ","日持ちする","職場用","自分用","家族・友人用"]);
-  const souvenirButtonNames = ["甘い","スナック系","個包装","あんこ","日持ちする","職場用","自分用","家族・友人用"]
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([
+    "甘い",
+    "スナック系",
+    "個包装",
+    "あんこ",
+    "日持ちする",
+    "職場用",
+    "自分用",
+    "家族・友人用",
+  ]);
+  const souvenirButtonNames = [
+    "甘い",
+    "スナック系",
+    "個包装",
+    "あんこ",
+    "日持ちする",
+    "職場用",
+    "自分用",
+    "家族・友人用",
+  ];
+  const [souvenirItems, setSouvenirItems] = useState([
+    { name: "南部せんべい", categories: ["個包装", "日持ちする"], company: "" },
+    { name: "盛岡冷麺", categories: [ "日持ちする"], company: "ぴょんぴょん舎" },
+    {
+      name: "岩手のはちみつ ラングドシャ",
+      categories: ["甘い"],
+      company: "山本養蜂場",
+    },
+    { name: "三陸えびせんべい", categories: ["スナック系"], company: "" },
+    { name: "かもめのたまご", categories: ["あんこ","甘い"], company: "さいとう製菓" },
+    { name: "ごま摺り団子", categories: ["あんこ"], company: "" },
+    { name: "Cava（サヴァ）缶", categories: ["日持ちする"], company: "" },
+    {
+      name: "Cava（サヴァ）缶",
+      categories: ["日持ちする"],
+      company: "小山製麺",
+    },
+  ]);
   const clickSouvenirCategory = (categoryName: string) => {
     const index = selectedCategories.indexOf(categoryName);
+    console.log(selectedCategories);
+    console.log(souvenirItems);
+
     if (index === -1) {
       setSelectedCategories([...selectedCategories, categoryName]);
     } else {
@@ -16,17 +56,43 @@ const SouvenirPage = () => {
       newCategories.splice(index, 1);
       setSelectedCategories(newCategories);
     }
-    console.log(selectedCategories);
   };
   return (
     <div className="w-11/12 mx-auto">
-      <h3 className={`${noto_sans_jp.className} font-semibold mt-6 ml-1`}>カテゴリ</h3>
+      <h3 className={`${noto_sans_jp.className} font-semibold mt-6 ml-1`}>
+        カテゴリ
+      </h3>
       <div className="flex flex-wrap gap-1.5 mt-6">
-        {souvenirButtonNames.map((souvenirButtonName,index) => {
-          return <SouvenirButton key={index} onClick={() => clickSouvenirCategory(souvenirButtonName)} name={souvenirButtonName} selected={selectedCategories.includes(souvenirButtonName)} />
+        {souvenirButtonNames.map((souvenirButtonName, index) => {
+          return (
+            <SouvenirButton
+              key={index}
+              onClick={() => clickSouvenirCategory(souvenirButtonName)}
+              name={souvenirButtonName}
+              selected={selectedCategories.includes(souvenirButtonName)}
+            />
+          );
         })}
       </div>
-
+      <div className="flex flex-wrap mt-4">
+        {/* アイテムエリア */}
+        {souvenirItems.map((item, index) => {
+          if (
+            selectedCategories.some((category) =>
+              item.categories.includes(category),
+            )
+          ) {
+            return (
+              <SouvenirItem
+                key={index}
+                name={item.name}
+                company={item.company}
+              ></SouvenirItem>
+            );
+          }
+          return null;
+        })}
+      </div>
     </div>
   );
 };

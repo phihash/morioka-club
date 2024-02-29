1895bc2af358dab47e503da8c8f9ee7e04649bde
phihash <insharp0220@gmail.com>
2024-02-29 15:49:50 +0900

    ogp設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,19 @@ import Header from "./components/header";
 export const metadata: Metadata = {
   title: "盛岡かんたんガイド",
   description: "盛岡かんたんガイド",
+  openGraph: {
+    title: "盛岡かんたんガイド",
+    description:"盛岡簡単ガイドLINE",
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "盛岡かんたんガイドtwitter",
+    description:"Twiiter説明",
+    site: '@hogehoge',
+    creator: '@hogehoge',
+  },
   icons: [
     {
       rel: "apple-touch-icon",
@@ -31,6 +44,10 @@ export default function RootLayout({
           type="image/<generated>"
           sizes="<generated>"
         />
+        <meta property="og:image" content="<generated>" />
+<meta property="og:image:type" content="<generated>" />
+<meta property="og:image:width" content="<generated>" />
+<meta property="og:image:height" content="<generated>" />
       </head>
       <body className={inter.className}>
         <Header></Header>

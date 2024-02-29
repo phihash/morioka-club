046504dddc153096181dddca417a5b66fc30b4f1
phihash <insharp0220@gmail.com>
2024-02-29 19:39:41 +0900

    foodページもおみやげのようにした

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,16 +1,56 @@
-import React from "react";
-import { fredoka } from "../fonts";
-import FoodContentBox from "../components/foodContentBox";
 
-const page = () => {
+"use client"
+import React from "react";
+import FoodItem from "../components/foodItem";
+import { noto_sans_jp } from "../fonts";
+import { useState } from "react";
+import FoodButton from "../components/foodButton";
+
+
+const FoodPage = () => {
+  const [selectedFoodCategories, setSelectedFoodCategories] = useState<string[]>([
+    "Wifi有",
+    "充電有",
+    "カフェ",
+    "入りやすい"
+  ]);
+  const foodButtonNames = [
+    "Wifi有",
+    "充電有",
+    "カフェ",
+    "入りやすい"
+  ];
+  const clickSouvenirCategory = (categoryName: string) => {
+    const index = selectedFoodCategories.indexOf(categoryName);
+
+    if (index === -1) {
+      setSelectedFoodCategories([...selectedFoodCategories, categoryName]);
+    } else {
+      const newCategories = [...selectedFoodCategories];
+      newCategories.splice(index, 1);
+      setSelectedFoodCategories(newCategories);
+    }
+  };
   return (
-    <div className="bg-lime-50">
-      <div className="w-9/12 mx-auto pt-12">
-        <h2 className={`${fredoka.className} text-4xl font-semibold`}>Food</h2>
+    <div className="w-11/12 mx-auto">
+      <h3 className={`${noto_sans_jp.className} font-semibold mt-6 ml-1`}>
+        カテゴリ
+      </h3>
+      <div className="flex flex-wrap gap-1.5 mt-6">
+        {foodButtonNames.map((foodButtonName, index) => {
+          return (
+            <FoodButton
+              key={index}
+              onClick={() => clickSouvenirCategory(foodButtonName)}
+              name={foodButtonName}
+              selected={selectedFoodCategories.includes(foodButtonName)}
+            />
+          );
+        })}
       </div>
-      <FoodContentBox></FoodContentBox>
+      <FoodItem dish="パスタ" restaurant="レストラン"></FoodItem>
     </div>
   );
 };
 
-export default page;
+export default FoodPage;

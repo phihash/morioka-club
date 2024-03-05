6424676641f3c859f08566daccf37767bca83c1d
phihash <insharp0220@gmail.com>
2024-03-05 17:31:59 +0900

    404ページ変更

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,36 @@
-import {  zen_maru_gothic  } from "./fonts";
+import { noto_sans_jp} from "./fonts";
 
 function NotFoundPage() {
-	return (
-    <div className="w-11/12 mx-auto">
-      <h2 className="w-max mx-auto pt-24 pb-12 text-9xl">😵‍💫</h2>
-      <p className="w-max mx-auto text-xl pb-12 font-bold">そのようなページご用意してません</p>
-      <p className={`w-max mx-auto text-5xl font-bold ${zen_maru_gothic.className}`}>404</p>
+  return (
+    <div className="bg-green-50 py-24">
+      {/* pb-24でした */}
+      <div className="px-8 py-16 md:w-3/4 w-10/12 mx-auto bg-white  rounded-xl">
+        <p
+          className={` md:text-4xl text-3xl font-semibold ${noto_sans_jp.className} w-max mx-auto mb-12`}
+        >
+          ページが見つかりません
+        </p>
+        <p
+          className={`md:text-8xl text-8xl font-semibold ${noto_sans_jp.className} w-max mx-auto mb-12`}
+        >
+          🤷‍♂️🤷‍♀️
+        </p>
+        <p
+          className={`text-base ${noto_sans_jp.className} font-semibold mb-8 md:text-center tracking-widest leading-7`}
+        >
+          こちらのサイトをご利用いただき、ありがとうございます。<br/>
+お探しのページは元から存在しないか、あるいは現在利用できない可能性があります。<br/>
+
+        </p>
+        <p
+          className={`text-base ${noto_sans_jp.className} font-semibold mb-6 md:text-center tracking-widest`}
+          >
+        お手数ですが、トップページから再度お探しくださいますよう、お願いいたします。
+
+        </p>
+      </div>
+
+
     </div>
   );
 }

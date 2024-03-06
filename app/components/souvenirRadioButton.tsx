fae024566068b553f5a974459e5651ff05e0a11e
phihash <insharp0220@gmail.com>
2024-03-06 19:38:44 +0900

    おみやげボタン追加

---
+++ app/components/souvenirRadioButton.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { noto_sans_jp } from "../fonts";
+
+const souvenirRadioButton: React.FC<SouvenirRadioButtonProps> = ({
+  name,
+  onClick,
+}) => {
+  return (
+    <div className="flex items-center cursor-pointer" onClick={onClick}>
+      <input
+        id={name}
+        type="radio"
+        value={name}
+        name="souvenir-sort"
+        className="appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-emerald-500 checked:border-lime-500 focus:outline-none cursor-pointer"
+      />
+      <label
+        htmlFor={name}
+        className={`${noto_sans_jp.className} ml-2 text-base font-medium text-gray-900 cursor-pointer`}
+      >
+        {name}
+      </label>
+    </div>
+  );
+};
+
+interface SouvenirRadioButtonProps {
+  name: string;
+  onClick: () => void;
+}
+
+export default souvenirRadioButton;

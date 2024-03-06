557c3bcb7e169330af2f83d36ad15354933f3a27
phihash <insharp0220@gmail.com>
2024-03-06 23:46:07 +0900

    wip

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -3,24 +3,26 @@ import { noto_sans_jp } from "../fonts";
 
 const DiaryPage = () => {
   return (
-    <div className="bg-green-50 pb-24">
-      {/* pb-24でした */}
-      <div className="py-24">
-        <p
-          className={`text-6xl font-semibold ${noto_sans_jp.className} w-max mx-auto mb-6`}
-        >
-          📔
-        </p>
-        <h1
-          className={`md:text-4xl text-3xl font-semibold ${noto_sans_jp.className} w-max mx-auto `}
-        >
-          体験記
-        </h1>
-      </div>
+    <div className="bg-green-700">
+      <div className="bg-white rounded-b-3xl  pb-24">
+        {/* pb-24でした */}
+        <div className="py-24">
+          <p
+            className={`text-6xl font-semibold ${noto_sans_jp.className} w-max mx-auto mb-6`}
+          >
+            📔
+          </p>
+          <h1
+            className={`md:text-4xl text-3xl font-semibold ${noto_sans_jp.className} w-max mx-auto `}
+          >
+            体験記
+          </h1>
+        </div>
 
-      <div
-        className={`md:w-3/4 w-10/12 mx-auto ${noto_sans_jp.className} bg-white rounded-xl`}
-      ></div>
+        <div className={`md:w-3/4 w-10/12 mx-auto ${noto_sans_jp.className} font-semibold`}>
+このページは、現在編集中です。
+        </div>
+      </div>
     </div>
   );
 };

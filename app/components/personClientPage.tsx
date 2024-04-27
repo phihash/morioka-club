6344b9489fc0b956bc816ae8d8666fd83fc05282
phihash <insharp0220@gmail.com>
2024-04-27 23:53:09 +0900

    personページ追加

---
+++ app/components/personClientPage.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { noto_sans_jp } from "../fonts";
+
+
+const PersonClientPage = () => {
+
+  return (
+    <div className="bg-green-700 ">
+      <div className="bg-white rounded-b-3xl">
+        <div className="w-11/12 mx-auto pb-12">
+          person
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PersonClientPage;
